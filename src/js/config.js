763740a8eff06405b67c2a739d6c0aadd36916d5
agelarakisd <agelarakisd@icloud.com>
2023-09-08 13:26:51 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '50c4db3e-a445-4382-a159-31fa82933a91';
+export const MODAL_CLOSE_SEC = 2.5;
