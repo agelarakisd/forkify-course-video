d57ab1e9386e1b2b1b1aa47d67729c704f0d5476
agelarakisd <agelarakisd@icloud.com>
2023-09-11 10:55:16 +0300

    fixed dependencies again

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '50c4db3e-a445-4382-a159-31fa82933a91';
+export const KEY = '04fb5406-6660-48a8-abd5-1f4c7cf978b9';
 export const MODAL_CLOSE_SEC = 2.5;
